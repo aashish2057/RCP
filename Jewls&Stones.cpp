73d40b30f362b4b526c589ec898b0b3bdc62ba6d
Aashish Sharma <aashish2057@gmail.com>
2019-01-23 10:39:10 -0600

    Finished Jewls&Stones 4ms - 98.65%

--- Jewls&Stones.cpp
+++ Jewls&Stones.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int numJewelsInStones(string J, string S) {
-        d 
+        char stone;
+        char jewel;
+        int counter;
+        for(int i = 0; i < S.length(); i++)
+        {
+            stone = S.at(i);
+            for(int x = 0; x < J.length(); x++)
+            {
+                jewel = J.at(x);
+                if(stone == jewel)
+                {
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 };
